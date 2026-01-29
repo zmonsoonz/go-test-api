6f18617575f8a0f8a1dd1499d1038286fb8c8e7f
Mokrov Danila <mok-danil28@mail.ru>
2026-01-30 01:57:33 +0300

    fix: correct migration

--- migrations/000002_deleted_unused.up.sql
+++ migrations/000002_deleted_unused.up.sql
@@ -1,30 +1,4 @@
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL,
-    password_hash VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS libraries (
-    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL,
-    
-    CONSTRAINT fk_libraries_user
-        FOREIGN KEY (user_id)
-        REFERENCES users(id)
-        ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS tracks (
-    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    library_id INTEGER NOT NULL,
-    description VARCHAR(255),
-    uploaded_at TIMESTAMPTZ NOT NULL,
-    
-    CONSTRAINT fk_tracks_library
-        FOREIGN KEY (library_id)
-        REFERENCES libraries(id)
-        ON DELETE CASCADE
-);
+ALTER TABLE libraries DROP COLUMN IF EXISTS total_tracks;
+ALTER TABLE tracks DROP COLUMN IF EXISTS duration;
+ALTER TABLE tracks DROP COLUMN IF EXISTS file_path;
+ALTER TABLE tracks ADD COLUMN description VARCHAR(255);
